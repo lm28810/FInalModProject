006947fddf0542e9c69a8795961c12778b62196e
Jazmine <lm28810@gmail.com>
2023-01-24 23:17:57 -0500

    backend stage one complete

---
+++ Backend/Routes/Item.js
@@ -0,0 +1,63 @@
+const router = require('express').Router();
+let Product = require('../models/Items.model')
+
+
+
+
+router.route('/').get((req, res) => {
+  Product.find()
+    .then(items => res.json(items))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/add').post((req, res) => {
+    const name = req.body.name;
+    const inventory = req.body.inventory;
+    const deliveryDate = Date.parse(req.body.deliveryDate);
+    const deliveryPrice = req.body.deliveryPrice;
+    const salesPrice = req.body.salesPrice;
+
+
+  const newProduct = new Product({
+      name,
+      inventory,
+      deliveryDate,
+      deliveryPrice,
+      salesPrice,
+  });
+
+  newProduct.save()
+  .then(() => res.json('Exercise added!'))
+  .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').get((req, res) => {
+  Product.findById(req.params.id)
+    .then(item => res.json(item))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').delete((req, res) => {
+  Product.findByIdAndDelete(req.params.id)
+    .then(() => res.json(' Deleted.'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/update/:id').post((req, res) => {
+  Product.findById(req.params.id)
+    .then(item => {
+            Product.name = req.body.name;
+            Product.inventory = req.body.inventory;
+            Product.deliveryDate = Date.parse(eq.body.deliveryDate);
+            Product.deliveryPrice = req.body.deliveryPrice;
+            Product.salesPrice = req.body.salesPrice;
+
+
+      product.save()
+        .then(() => res.json('Exercise updated!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+    })
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+module.exports = router;
---
+++ Backend/server.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+require('dotenv').config();
+const Items = require('./models/Items.model')
+const productRouter = require('./Routes/Item')
+
+
+const app = express()
+const port = process.env.PORT || 4000;
+app.use(cors())
+
+//console.log(Items)
+
+app.use(express.json())
+
+//app.use(express.static('public'))
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect( uri, 
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+);
+
+mongoose.connection.once('open', ()=> {
+    console.log('connected to mongo');
+});
+
+// Route to Create Items.create()
+
+  app.use('/Items', productRouter)
+
+
+
+
+app.listen(port, ()=>{console.log(`running on ${port}`)})
+
