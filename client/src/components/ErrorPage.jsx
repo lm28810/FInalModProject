108989925cdb4f31e3d9914ac1645a750cb5fb04
Jazmine <lm28810@gmail.com>
2023-01-29 13:03:02 -0500

    errorpage done and responsive

--- client/src/components/ErrorPage.jsx
+++ client/src/components/ErrorPage.jsx
@@ -1,16 +1,19 @@
 import { useRouteError } from 'react-router-dom'
 
 import React from 'react'
+import './ErrorPage.css'
 
 function ErrorPage() {
     const error = useRouteError()
     console.error(error)
   return (
-      <div>
+      <div className='container'>
+          <div className='error-info'>
           <h1>Oops!</h1>
           <p>Sorry, an unexpected error has occurred.</p>
-          <i>{error.statusText || error.message} </i>
-    </div>
+         <p> <i className='error'>{error.statusText || error.message} </i></p>
+          </div>
+      </div>
   )
 }
 
