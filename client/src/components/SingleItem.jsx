5c88174a6e53ab81efd077ed13457d53d17728b1
Jazmine <lm28810@gmail.com>
2023-02-03 17:40:10 -0500

    single card working

--- client/src/components/SingleItem.jsx
+++ client/src/components/SingleItem.jsx
@@ -23,14 +23,16 @@ function SingleItem() {
       <div className='Scard'>Entire Card
         <div>
           <h2 className='ProductName'>{record.productname}</h2>
-          <div className='current-image'></div>
+          <div className='current-image'>
+            <img src={record.avatar} alt={record.productname} />
+          </div>
         </div>
         <div className='Single-info-container'>
           <h3 className='single-title'>Product Info</h3>
           <ul>
             <li>{`Price ${record.price}`}</li>
-            <li>`Delivery Price ` </li>
-            <li>Avaliable for Delivery</li>
+            <li>Delivery Price: {record.deliveryAmt} </li>
+            <li>Avaliable for Delivery: { record.nextDelivery}</li>
             <li></li>
             <li></li>
             <li><p>{ record.description} Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia excepturi velit beatae, in dicta perspiciatis? Labore optio minus delectus neque error eos sit molestiae suscipit dolore. Cum officiis ut iure.</p></li>
